0da0549ac13faee9f383bbdccf3264b0403abbe2
Rabbouz <rabbouzzakr@gmail.com>
2023-06-15 12:50:35 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry  point
+ *
+ * Description: A C program that prints a text using put function
+ *
+ * Return: 0 Always (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
