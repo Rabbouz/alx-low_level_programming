4ac851de1b4a10ba8d16706e95455409be3af111
Rabbouz <rabbouzzakr@gmail.com>
2023-06-15 13:27:24 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point
+ *
+ * Description: A program that prints a text using printf function
+ *
+ * Return: 0 Always (Success)
+*/
+
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
