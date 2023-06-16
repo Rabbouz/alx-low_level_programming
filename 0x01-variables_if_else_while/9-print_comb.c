069431fa73e3aeeb122f3d244fbb1eaf734f2b07
Rabbouz <rabbouzzakr@gmail.com>
2023-06-16 14:08:22 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main- Entry Point
+ *
+ * Description: printing all possible combinations of single-digit numbers
+ * usibg putchar
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+{
+	putchar(i);
+	putchar(',');
+	putchar(' ');
+}
+putchar('\n');
+return (0);
+}
