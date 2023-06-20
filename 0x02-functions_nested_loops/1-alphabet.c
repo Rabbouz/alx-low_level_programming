da374de7d20ed02f2620bd5b4c7a557ea7eb2495
Rabbouz <rabbouzzakr@gmail.com>
2023-06-20 08:48:31 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main- Entry point
+ *
+ * Description: printing the alphabet, in lowercase
+ *
+ * Return: Void
+*/
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
