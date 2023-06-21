6f0c3f32fa6021c462b1a75af68f268c432a18c8
Rabbouz <rabbouzzakr@gmail.com>
2023-06-21 09:12:24 -0700

    debugging multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  positive_or_negative - printing integer number
+ *
+ * @i : integer
+*/
+
+void positive_or_negative(int i)
+
+{
+
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
+}
+
