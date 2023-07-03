9242136ec8a285e4e2b3c4fa62f9875232069bd0
Rabbouz <rabbouzzakr@gmail.com>
2023-07-03 11:19:07 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - Write a function that locates
+ * a character in a string
+ * @s: pointer input
+ * @c: constant input
+ * Return: poiter s
+ */
+
+char *_strchr(char *s, char c)
+
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0' ; i++)
+		{
+			if (s[i] == c)
+			{
+				return (s + i);
+			}
+		}
+	return ('\0');
+}
