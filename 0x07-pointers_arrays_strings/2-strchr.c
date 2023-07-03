b05c7323630df7297487b4554a9b73619f159703
Rabbouz <rabbouzzakr@gmail.com>
2023-07-03 11:32:26 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,20 +5,17 @@
  * a character in a string
  * @s: pointer input
  * @c: constant input
- * Return: poiter s
+ * Return: pointer s
  */
 
 char *_strchr(char *s, char c)
-
 {
 	int i;
 
 	for (i = 0; s[i] >= '\0' ; i++)
 		{
-			if (s[i] == c)
-			{
-				return (s + i);
-			}
-		}
-	return ('\0');
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
 }
