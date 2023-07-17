1a2cd32f95c9eb818510de12734722a341caa41e
Rabbouz <rabbouzzakr@gmail.com>
2023-07-17 14:12:15 -0700

    free dogs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,13 +3,17 @@
 
 /**
  * free_dog - A function that frees dogs =)
- * @d: struct dog to free
+ * @d: struct dog(pinter) to free
+ *
+ *Return: void
  */
 
 void free_dog(dog_t *d)
 {
 	if (d)
+	{
 		free(d->name);
-	free(d);
-	free(d->owner);
+		free(d->owner);
+		free(d);
+	}
 }
