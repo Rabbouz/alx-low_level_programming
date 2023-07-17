4d1f1741fb25f09f8fea9941c9bbda1047a8356a
Rabbouz <rabbouzzakr@gmail.com>
2023-07-17 13:31:27 -0700

    dog-h update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,16 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_tp - typedef for struct dog
+ */
+typedef struct dog dog_tp;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_tp *new_dog(char *name, float age, char *owner);
+void free_dog(dog_tp *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 #endif
