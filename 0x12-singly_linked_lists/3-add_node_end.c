e0aab6b100b9adac906d7727db25ab0ad0a02cc9
Rabbouz <rabbouzzakr@gmai.com>
2023-07-27 16:51:33 -0700

    update add_node task3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -25,21 +25,23 @@ list_t *add_node_end(list_t **head, const char *str)
 		len++;
 
 	new_node = malloc(sizeof(list_t));
+
+	if (!new_node)
+	return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = len;
+	new_node->next = NULL;
+
 	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
-	if (!new_node)
-	{
-		return (NULL);
-	}
 
-	new_node->str = strdup(str);
-	new_node->len = len;
-	new_node->next = NULL;
 	while (temp->next)
 		temp = temp->next;
 	temp->next = new_node;
+
 	return (new_node);
 }
