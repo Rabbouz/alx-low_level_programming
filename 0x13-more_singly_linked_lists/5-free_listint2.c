aeed9faf7f88ba83c0f5736de9834d47eb25f03a
Rabbouz <rabbouzzakr@gmai.com>
2023-08-01 12:19:45 -0700

    update free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,14 +11,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tp;
 
-	if if (head == NULL || *head == NULL)
+	if (head == NULL || *head == NULL)
 		return;
 
 	while (*head)
 	{
-		temp = (*head)->next;
+		tp = (*head)->next;
 		free(*head);
-		*head = temp;
+		*head = tp;
 	}
 
 	*head = NULL;
