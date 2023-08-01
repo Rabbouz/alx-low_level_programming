e6e84bb0b4a217ac39c84beaba67d712d9601c0a
Rabbouz <rabbouzzakr@gmai.com>
2023-08-01 12:09:08 -0700

    free list update

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - A function that frees a linked list
+ * @head: list to be freed(listoint_t)
+ *
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tp;
+
+	while (head != NULL)
+	{
+		tp = head->next;
+		tp = head;
+		free(tp);
+	}
+}
