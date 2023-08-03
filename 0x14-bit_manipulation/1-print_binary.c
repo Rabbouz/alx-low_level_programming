c8e683e1d26bb528181fe75594e5669ccc5abcde
Rabbouz <rabbouzzakr@gmai.com>
2023-08-03 03:24:57 -0700

    1st

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+include "main.h"
+/**
+ * print_binary - A function that prints
+ * the binary equivalent of a decimal number
+ * @n: printed number
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	int it, counter;
+	
+	counter = 0;
+
+	unsigned long int curr;
+
+	for (it = 63; it >= 0; it--)
+	{
+		curr = n >> it;
+
+		if (curr & 1t)
+		{
+			_putchar('1');
+			counter++;
+		}
+		else if (counter)
+			_putchar('0');
+	}
+	if (!counter)
+		_putchar('0');
+}
