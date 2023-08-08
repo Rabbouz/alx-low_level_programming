c1ad5156fedbec779026f043a7adc858c8ab120a
rabbouz <rabbouzzakr@gmail.com>
2023-08-08 02:50:41 +0100

    100-elf_header.c commit

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -135,28 +135,42 @@ void print_file_version(unsigned char *magic_numbers)
 void print_os_and_abi(unsigned char *magic_numbers)
 {
 	printf(" OS/ABI: ");
-	if (magic_numbers[EI_OSABI] == ELFOSABI_NONE)
+
+	switch (magic_numbers[EI_OSABI])
+	{
+	case ELFOSABI_NONE:
 		printf("UNIX - System V\n");
-		else if (magic_numbers[EI_OSABI] == ELFOSABI_HPUX)
-			printf("UNIX - HP-UX\n");
-			else if (magic_numbers[EI_OSABI] == ELFOSABI_NETBSD)
-				printf("UNIX - NetBSD\n");
-				else if (magic_numbers[EI_OSABI] == ELFOSABI_LINUX)
-					printf("UNIX - Linux\n");
-					else if (magic_numbers[EI_OSABI] == ELFOSABI_SOLARIS)
-						printf("UNIX - Solaris\n");
-						else if (magic_numbers[EI_OSABI] == ELFOSABI_IRIX)
-							printf("UNIX - IRIX\n");
-							else if (magic_numbers[EI_OSABI] == ELFOSABI_FREEBSD)
-								printf("UNIX - FreeBSD\n");
-								else if (magic_numbers[EI_OSABI] == ELFOSABI_TRU64)
-									printf("UNIX - TRU64\n");
-									else if (magic_numbers[EI_OSABI] == ELFOSABI_ARM)
-										printf("ARM\n");
-										else if (magic_numbers[EI_OSABI] == ELFOSABI_STANDALONE)
-										printf("Standalone App\n");
-										else
-											printf("<unknown: %x>\n", magic_numbers[EI_OSABI]);
+		break;
+	case ELFOSABI_HPUX:
+		printf("UNIX - HP-UX\n");
+		break;
+	case ELFOSABI_NETBSD:
+		printf("UNIX - NetBSD\n");
+		break;
+	case ELFOSABI_LINUX:
+		printf("UNIX - Linux\n");
+		break;
+	case ELFOSABI_SOLARIS:
+		printf("UNIX - Solaris\n");
+		break;
+	case ELFOSABI_IRIX:
+		printf("UNIX - IRIX\n");
+		break;
+	case ELFOSABI_FREEBSD:
+		printf("UNIX - FreeBSD\n");
+		break;
+	case ELFOSABI_TRU64:
+		printf("UNIX - TRU64\n");
+		break;
+	case ELFOSABI_ARM:
+		printf("ARM\n");
+		break;
+	case ELFOSABI_STANDALONE:
+		printf("Standalone App\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", magic_numbers[EI_OSABI]);
+	}
 }
 /**
  * print_abi_version - A function that prints the ABI version of an ELF.
