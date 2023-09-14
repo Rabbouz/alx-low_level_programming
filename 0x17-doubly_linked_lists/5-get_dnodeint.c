491e6b2fd2a68d7398e9bc8d5a27f6bdcac42a5d
Rabbouz <rabbouzzakr@gmail.com>
2023-09-14 23:35:29 +0100

    Get node at index update

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -7,22 +7,20 @@
  */
 
 int sum_dlistint(dlistint_t *head)
-
 {
 	int sum = 0;
 
 	if (head != NULL)
 	{
-		return (sum);
+		while (head->prev != NULL)
+			head = head->prev;
+
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
 	}
-	while (head->prev != NULL)
-	{
-		head = head->prev;
-	}
-	while (head != NULL)
-	{
-		sum += head->n;
-		head = head->next;
-	}
+
 	return (sum);
 }
